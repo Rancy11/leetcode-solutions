54a3219bab71cd5d689bb23ecf662011d1abb486
Rancy <157047320+Rancy11@users.noreply.github.com>
2024-12-03 12:33:01 +0000

    Sync LeetCode submission - Greatest Common Divisor of Strings - Runtime - 0 ms (100.00%), Memory - 9.9 MB (34.13%)

---
+++ solutions/1146-greatest-common-divisor-of-strings/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        string ans;
+        if((str1+str2) != (str2+str1))
+        return "";
+        int len= gcd(str1.length(), str2.length());
+        ans= str1.substr(0,len);
+        return ans;
+    }
+};
