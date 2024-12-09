a90fe4982dd0b98c655b523165689cc73bbcbdc5
Rancy <157047320+Rancy11@users.noreply.github.com>
2024-12-09 12:33:32 +0000

    Sync LeetCode submission - Reverse Vowels of a String - Runtime - 1 ms (48.03%), Memory - 10.2 MB (91.03%)

---
+++ solutions/0345-reverse-vowels-of-a-string/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        auto isVowel = [](char c) {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+        };
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (!isVowel(s[i])) {
+                i++;
+                continue;
+            }
+            if (!isVowel(s[j])) {
+                j--;
+                continue;
+            }
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+        return s;
+    }
+};
