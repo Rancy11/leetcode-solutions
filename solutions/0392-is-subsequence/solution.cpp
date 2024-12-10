6b31bb79af99e248ca92b312223ba3762c83b623
Rancy <157047320+Rancy11@users.noreply.github.com>
2024-12-10 14:01:06 +0000

    Sync LeetCode submission - Is Subsequence - Runtime - 0 ms (100.00%), Memory - 8.7 MB (32.49%)

---
+++ solutions/0392-is-subsequence/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j=0;
+        for (int i=0; i<t.length(); i++){
+            if(s[j] == t[i]){
+                j++;
+            }
+            if(j == s.size()){
+                return true;
+            }
+        }
+         if(s==""){
+                return true;
+            }
+         return false;
+    
+    }
+};
