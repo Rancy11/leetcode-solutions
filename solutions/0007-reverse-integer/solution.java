76000630ac9860afb8b9b2e567ce8f68bb77e36a
Rancy <157047320+Rancy11@users.noreply.github.com>
2025-03-04 07:44:27 +0000

    Sync LeetCode submission - Reverse Integer - Runtime - 0 ms (100.00%), Memory - 41.4 MB (99.97%)

---
+++ solutions/0007-reverse-integer/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+        int revN=0;
+        while(x!=0){
+            int lastdigit= x%10;
+            x= x/10;
+            if (revN > Integer.MAX_VALUE / 10 || revN < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            revN= (revN*10)+lastdigit;
+        }
+        return revN;
+    }
+}
