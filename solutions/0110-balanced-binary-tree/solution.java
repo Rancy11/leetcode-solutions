4276f55edef32368c72a96a0699158752ed79ccd
Rancy <157047320+Rancy11@users.noreply.github.com>
2025-10-28 08:31:04 +0000

    Sync LeetCode submission - Balanced Binary Tree - Runtime - 0 ms (100.00%), Memory - 44.6 MB (99.91%)

---
+++ solutions/0110-balanced-binary-tree/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    private int checkHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = checkHeight(node.left);
+        int rightHeight = checkHeight(node.right);
+
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
