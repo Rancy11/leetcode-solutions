7bd6fb661772f4b32151561d79e17fc404602e64
Rancy <157047320+Rancy11@users.noreply.github.com>
2025-11-25 16:04:12 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array - Runtime - 1 ms (78.18%), Memory - 46.8 MB (54.38%)

--- solutions/0026-remove-duplicates-from-sorted-array/solution.java
+++ solutions/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,14 +1,18 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length==0) return 0;
-        int i=0;
-
-        for(int j=1; j<nums.length; j++){
-            if(nums[i]!=nums[j]){
-                i++;
-                nums[i]=nums[j];
+        int officer = 0;
+        int res=1;
+        int cm = 1;
+        while(cm<nums.length){
+            if(nums[cm]== nums[cm-1] ){
+                cm++;
+                continue;
             }
+            nums[officer+1] = nums[cm];
+            officer++;
+            res++;
+            cm++; 
         }
-        return i+1;
+        return res;
     }
 }
