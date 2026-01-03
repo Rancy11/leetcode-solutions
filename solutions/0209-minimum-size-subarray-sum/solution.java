86a8031a8a60a6f04c842feddf44b8fb969f7e6d
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-01-03 12:13:18 +0000

    Sync LeetCode submission - Minimum Size Subarray Sum - Runtime - 2 ms (16.24%), Memory - 62.3 MB (23.96%)

--- solutions/0209-minimum-size-subarray-sum/solution.java
+++ solutions/0209-minimum-size-subarray-sum/solution.java
@@ -1,23 +1,19 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int n= nums.length;
-        int i=0; 
-        int j=0;
-        int sum= nums[0];
-        int minSize= Integer.MAX_VALUE;
-
-        while(j<n){
-            if(i==j && nums[i]>=target) return 1;
-            else if(i!=j && sum>= target){
-                minSize= Math.min(minSize, j-i+1);
-                sum-= nums[i];
-                i++;
+        int low =0, high =0;
+        int sum = 0;
+        int res = Integer.MAX_VALUE;
+        int n = nums.length;
+        while(high < n){
+            sum+= nums[high];
+            while(sum >= target){
+                int len = high-low+1;
+                res = Math.min(res, len);
+                sum -= nums[low];
+                low++;
             }
-            else{
-                j++;
-                if(j<n)sum+=nums[j];
-            }
+            high++;
         }
-        return minSize == Integer.MAX_VALUE ? 0 : minSize; 
+        return res== Integer.MAX_VALUE ? 0 : res;
     }
 }
