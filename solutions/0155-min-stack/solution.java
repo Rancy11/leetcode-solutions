97cbb380659493a66f332041d0edadc0a9b7f649
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-01-28 15:30:25 +0000

    Sync LeetCode submission - Min Stack - Runtime - 8 ms (6.44%), Memory - 47.3 MB (21.18%)

--- solutions/0155-min-stack/solution.java
+++ solutions/0155-min-stack/solution.java
@@ -1,19 +1,16 @@
 class MinStack {
     private Stack<Integer> stack;
     private Stack<Integer> minStack;
-
     public MinStack() {
         stack = new Stack<>();
-        minStack = new  Stack<>();      
+        minStack = new Stack<>();
     }
     
     public void push(int val) {
-
         stack.push(val);
-        if(minStack.isEmpty() || val <= minStack.peek()){
+        if(minStack.isEmpty() || val<= minStack.peek()){
             minStack.push(val);
         }
-        
     }
     
     public void pop() {
@@ -24,12 +21,10 @@ class MinStack {
     
     public int top() {
         return stack.peek();
-     
     }
     
     public int getMin() {
-       return   minStack.peek();
-        
+        return minStack.peek();
     }
 }
 
