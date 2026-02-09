6f4170da2ab92e9e14df8de2302805fbc19c96f9
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-02-09 09:37:03 +0000

    Sync LeetCode submission - Maximum Sum Circular Subarray - Runtime - 7 ms (47.14%), Memory - 51.1 MB (71.46%)

---
+++ solutions/0954-maximum-sum-circular-subarray/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        int maxSum = nums[0];
+        int minSum = nums[0];
+        int currMin = nums[0];
+        int currMax = nums[0];
+        int totalSum= nums[0];
+        for(int i=1; i<nums.length; i++){
+            totalSum+= nums[i];
+            currMax = Math.max(currMax + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, currMax);
+
+            currMin = Math.min(currMin + nums[i], nums[i]);
+            minSum = Math.min(minSum, currMin);
+        }
+        if(maxSum<0) return maxSum;
+        return Math.max(maxSum, totalSum - minSum);
+    }
+}
