8bc2d27212bb229d5656eb166d62a66cbe3a3af9
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-02-12 09:23:25 +0000

    Sync LeetCode submission - Minimum Common Value - Runtime - 2 ms (92.10%), Memory - 80.5 MB (44.92%)

---
+++ solutions/2634-minimum-common-value/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int left = 0;
+        int right = 0;
+        while(left < nums1.length && right < nums2.length){
+            if(nums1[left] < nums2[right]){
+                left++;
+            }
+            else if(nums2[right] < nums1[left]){
+                right++;
+            }
+            else{
+                return nums1[left];
+            }
+        }
+        return -1;
+    }
+}
