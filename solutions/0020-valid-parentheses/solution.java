6db69759b12680ee689143a1545795e1d7054a4e
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-02-23 05:37:17 +0000

    Sync LeetCode submission - Valid Parentheses - Runtime - 3 ms (87.41%), Memory - 43.6 MB (21.10%)

--- solutions/0020-valid-parentheses/solution.java
+++ solutions/0020-valid-parentheses/solution.java
@@ -2,19 +2,23 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
         for(char ch: s.toCharArray()){
-            if(ch == '(' || ch == '{' || ch== '['){
+            if(ch == '{' || ch == '(' || ch == '['){
                 st.push(ch);
             }
             else{
                 if(st.isEmpty()) return false;
-                char top = st.pop();
-                if(( ch== ')' && top != '(') ||
-                    ( ch== '}' && top != '{') ||
-                    ( ch== ']' && top != '[')){
-                        return false;
-                    } 
+                if ((ch == ')' && st.peek() == '(') ||
+                    (ch == '}' && st.peek() == '{') ||
+                    (ch == ']' && st.peek() == '[')) {
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
             }
+            
         }
         return st.isEmpty();
+
     }
 }
