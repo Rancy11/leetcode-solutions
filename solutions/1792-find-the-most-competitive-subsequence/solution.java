e461553d18f79816bbd2fb6740f67d31f636cd90
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-02-27 17:15:54 +0000

    Sync LeetCode submission - Find the Most Competitive Subsequence - Runtime - 69 ms (20.08%), Memory - 110.7 MB (82.56%)

---
+++ solutions/1792-find-the-most-competitive-subsequence/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] mostCompetitive(int[] nums, int k) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        for(int i=0; i<n; i++){
+            while(!st.isEmpty() && st.peek()> nums[i] && st.size() -1 + (n-i) >= k){
+                st.pop();
+            }
+            if(st.size()<k){
+                st.push(nums[i]);
+            }
+        }
+        int []ans = new int [k];
+        for(int i= k-1; i>=0; i--){
+            ans[i] = st.pop();
+        }
+        return ans;
+    }
+}
