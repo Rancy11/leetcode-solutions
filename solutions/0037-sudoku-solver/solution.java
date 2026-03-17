61532d3ad3e45ec489c15fb28eaea4d1077cb750
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-03-17 13:22:51 +0000

    Sync LeetCode submission - Sudoku Solver - Runtime - 212 ms (66.04%), Memory - 43.1 MB (26.16%)

---
+++ solutions/0037-sudoku-solver/solution.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+        
+    }
+
+    private boolean solve(char[][] board){
+        for(int row=0;row<9;row++){
+            for(int col=0;col<9;col++){
+                if(board[row][col]=='.'){
+                    for(char num='1';num<='9';num++){
+                        if(isValid(board,row,col,num)){
+
+                            board[row][col]=num;
+                        
+                        if(solve(board))
+                        return true;
+
+                        board[row][col]='.';
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+    }
+
+
+    private boolean isValid(char[][] board,int row,int col,char num){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==num) return false;
+
+            if(board[i][col]==num) return false;
+            
+        }
+            int rowR = row-row%3;
+            int colR = col-col%3;
+
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    if(board[rowR+i][colR+j]==num) return false;
+                }
+            }
+            return true;
+
+
+        
+    }
+}
