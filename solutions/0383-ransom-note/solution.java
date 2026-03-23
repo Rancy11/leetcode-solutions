8550e6ff96187b418fdfb02d8be4d722a8474ca4
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-03-23 11:56:13 +0000

    Sync LeetCode submission - Ransom Note - Runtime - 1 ms (99.93%), Memory - 45.9 MB (98.03%)

--- solutions/0383-ransom-note/solution.java
+++ solutions/0383-ransom-note/solution.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
+        int arr[] = new int[26];
         for(int i = 0 ; i<magazine.length(); i++){
             char ch = magazine.charAt(i);
-            map.put(ch, map.getOrDefault(ch,0)+1);
+            arr[ch- 'a']++;
         }
-        for(int i = 0 ; i<ransomNote.length(); i++){
+        for(int i=0; i< ransomNote.length(); i++){
             char ch = ransomNote.charAt(i);
-            map2.put(ch, map2.getOrDefault(ch,0)+1);
-        }
-        for(char ch : map2.keySet()){
-            if (map.getOrDefault(ch, 0) < map2.get(ch)) {
+            if(--arr[ch - 'a'] < 0){
                 return false;
             }
         }
         return true;
+        
     }
 }
