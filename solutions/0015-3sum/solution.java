49d8d83f69fd2f6ac4bcee4544988b6bd51700cf
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-03-26 07:38:48 +0000

    Sync LeetCode submission - 3Sum - Runtime - 36 ms (30.45%), Memory - 58.6 MB (94.23%)

--- solutions/0015-3sum/solution.java
+++ solutions/0015-3sum/solution.java
@@ -3,27 +3,25 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         int n = nums.length;
-
-        for(int i=0; i<n-2; i++){
-            if(i>0 && nums[i-1] == nums[i]){
+        for(int i = 0; i< n-2; i++){
+            if(i>0 && nums[i-1]== nums[i]){
                 continue;
             }
             int left = i+1;
             int right = n-1;
-            int sum = -1* nums[i];
-            while(left<right){
+            int sum = -1 * nums[i];
+            while(left < right){
                 int s = nums[left] + nums[right];
-                if(s == sum){
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                if(s == sum) {
+                    result.add(Arrays.asList( nums[i], nums[left], nums[right]));
                     left++;
                     right--;
-                    while(left<n && nums[left] == nums[left-1]){
+                    while(left< n && nums[left]== nums[left-1]){
                         left++;
                     }
-                    while(right>=0 && nums[right]== nums[right+1]){
+                    while(right>0 && nums[right] == nums[right+1]){
                         right--;
                     }
-
                 }
                 else if(s< sum){
                     left++;
