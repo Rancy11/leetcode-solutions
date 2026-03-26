4660ddc162540875631ddd492155733ef1f4e191
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-03-26 08:18:31 +0000

    Sync LeetCode submission - 3Sum Closest - Runtime - 17 ms (76.56%), Memory - 45.4 MB (75.12%)

--- solutions/0016-3sum-closest/solution.java
+++ solutions/0016-3sum-closest/solution.java
@@ -3,19 +3,19 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         int closest = nums[0] + nums[1] + nums[2];
-        for(int i=0 ; i<n-2 ; i++){
-            int left = i+1; 
+        for(int i=0; i<n-2 ;i++){
+            int left = i+1;
             int right = n-1;
-            while(left<right){
-                int sum = nums[left]+ nums[right]+ nums[i];
-                if(Math.abs(sum - target) < Math.abs(closest - target)) {
+            while(left< right){
+                int sum = nums[i]+ nums[left] + nums[right];
+                if(Math.abs(sum - target) < Math.abs(closest - target)){
                     closest = sum;
                 }
-                if(sum< target){
+                if(sum < target){
                     left++;
                 }
-                else if( sum>target){
-                    right--;
+                else if(sum > target){
+                    right --;
                 }
                 else{
                     return sum;
