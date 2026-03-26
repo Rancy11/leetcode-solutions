5d8add12e92777be4a01698d02a7525878f30429
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-03-26 13:55:12 +0000

    Sync LeetCode submission - Move Zeroes - Runtime - 4 ms (10.21%), Memory - 47.6 MB (83.22%)

--- solutions/0283-move-zeroes/solution.java
+++ solutions/0283-move-zeroes/solution.java
@@ -1,22 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n= nums.length;
-        int j=-1;
-        int i;
-        for( i=0; i<n; i++){
-            if(nums[i]==0){
-                j=i;
-                break;
+        int n = nums.length;
+        int left = 0;
+        for(int right = 0; right < n ; right++){
+            if(nums[right] != 0){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
             }
         }
-        if(j==-1) return;
-        for(i=j+1; i<n; i++){
-            if(nums[i]!=0){
-               int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
-            }
-        }
+        System.out.print(nums);
     }
 }
