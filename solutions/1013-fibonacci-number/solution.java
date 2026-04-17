4aaf373a5e73ce45d281c1e6461eb2cf7f64f83a
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-04-17 12:42:29 +0000

    Sync LeetCode submission - Fibonacci Number - Runtime - 0 ms (100.00%), Memory - 41.9 MB (68.83%)

--- solutions/1013-fibonacci-number/solution.java
+++ solutions/1013-fibonacci-number/solution.java
@@ -1,8 +1,13 @@
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
+        int [] dp = new int [n+1];
+        return solve(n, dp);
+    }
+    private int solve(int n, int[] dp){
+        if(n<=1) return n;
+        if(dp[n] != 0) return dp[n];
+        dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+        return dp[n];
 
-        return fib(n-1) + fib(n-2);
     }
 }
