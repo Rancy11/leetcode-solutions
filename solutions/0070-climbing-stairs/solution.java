bc003c543810ca714773f7a1ef1b7f0bd4edb924
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-05-08 19:50:11 +0000

    Sync LeetCode submission - Climbing Stairs - Runtime - 0 ms (100.00%), Memory - 42.2 MB (35.03%)

--- solutions/0070-climbing-stairs/solution.java
+++ solutions/0070-climbing-stairs/solution.java
@@ -1,17 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
-        return rec(n, dp);
-    }
-    private int rec( int n, int dp[]){
-        if(n == 0) return 1;
-        if(n == -1) return 0;
-        if(dp[n] != -1) return dp[n];
-        int one_step = rec(n-1, dp);
-        int two_step = rec(n-2, dp);
-        dp[n] = one_step+ two_step;
-        return dp[n];
-        
+        if(n<=2) return n;
+        int prev2 = 1;
+        int prev1 = 2;
+        for(int i= 3; i<=n ; i++){
+            int curr = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 }
