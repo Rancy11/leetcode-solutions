1a05518909837ca412217320213a478dee6335da
Rancy <157047320+Rancy11@users.noreply.github.com>
2026-05-08 15:53:02 +0000

    Sync LeetCode submission - Construct Binary Tree from Inorder and Postorder Traversal - Runtime - 1 ms (98.17%), Memory - 46.4 MB (46.96%)

---
+++ solutions/0106-construct-binary-tree-from-inorder-and-postorder-traversal/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int postIndex;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        postIndex = inorder.length - 1;
+        for(int i=0; i<inorder.length; i++){
+            map.put(inorder[i], i);
+        }
+        return helper(postorder, 0, inorder.length - 1);
+    }
+    private TreeNode helper(int[] postorder, int start, int end ){
+        if(start > end) return null;
+        int rootVal = postorder[postIndex--];
+        TreeNode root = new TreeNode(rootVal);
+        int index = map.get(rootVal);
+        root.right = helper(postorder, index+1, end);
+        root.left = helper(postorder, start, index - 1);
+        return root;
+    }
+}
